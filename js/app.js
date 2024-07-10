3e424de2b1cb6d3856fae9b84b089644a56b162b
Crepekun <105583922+Crepekun@users.noreply.github.com>
2024-07-10 22:16:46 +0900

    Update app.js

--- js/app.js
+++ js/app.js
@@ -101,3 +101,12 @@ answerContainer2.appendChild(answerButton2);
             alert("準備中です");
         })
     });
+
+
+    <!-- r, rr, sr, srr, ssr -->
+    <!-- 72, 24, 8, 4, 1 -->
+    function rarity_calculation(r, rr, sr, srr, rarity) {
+        const count_value = document.getElementById('count').value = Math.round(1);
+        const necessary_rarity_value = document.getElementById('necessary_rarity').value = Math.round(1);
+        const necessary_diamond_value = document.getElementById('necessary_diamond').value = Math.round(1);
+    }
