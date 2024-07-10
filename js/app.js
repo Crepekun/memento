60ee9aeb4dc5476c37620a86e36e10e8b2e82bb2
Crepekun <105583922+Crepekun@users.noreply.github.com>
2024-07-10 22:22:56 +0900

    Update app.js

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
     <!-- r, rr, sr, srr, ssr -->
     <!-- 72, 24, 8, 4, 1 -->
     function rarity_calculation(r, rr, sr, srr, rarity) {
-        const count_value = document.getElementById('count').value = Math.round(1);
-        const necessary_rarity_value = document.getElementById('necessary_rarity').value = Math.round(1);
-        const necessary_diamond_value = document.getElementById('necessary_diamond').value = Math.round(1);
+        const count_value = document.getElementById('count').value = Math.round(r*1+rr*4+sr*8+srr*24);
+        const necessary_rarity_value = document.getElementById('necessary_rarity').value = Math.round(72-count);
+        const necessary_diamond_value = document.getElementById('necessary_diamond').value = Math.round(necessary_rarity/3*660);
     }
